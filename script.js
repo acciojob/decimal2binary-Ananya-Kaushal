c3f1d88018152140950f8783af2c2a9ef17651bc
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-09-13 21:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
-	let a;
-	while(num>0)
+	let num=7;
+let a=0;
+let i=1;
+	while(num!=0)
 		{
 			let r=num%2;
-			a=a+r;
-			num=num/2;
+			a=a+r*i;
+			num=parseInt(num/2);
+			i=i*10;
 		}
 	console.log(a);
   
